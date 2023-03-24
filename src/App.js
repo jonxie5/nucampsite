a9824ad57766b804be3c4a0f26e95a2a2851ca48
jonxie <jon.xie@outlook.com>
2023-03-24 08:44:46 -0700

    update to hooks steps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList';
+//import CampsitesList from './features/campsites/CampsitesList';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <CampsitesList />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
