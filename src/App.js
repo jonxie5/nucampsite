65b75fb61e7f21c17377ea7feebfb144e0482fcd
Jon Xie <jon.xie@outlook.com>
2023-03-25 12:28:07 -0700

    post workshop commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -11,12 +12,13 @@ function App() {
     return (
         <div className='App'>
             <Header />
-                <Routes>
-                    <Route path='/' element={<HomePage />} />
-                    <Route path='contact' element={<ContactPage />} />
-                    <Route path='directory' element={<CampsitesDirectoryPage />} />
-                    <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
-                </Routes>
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+            </Routes>
             <Footer />
         </div>
     );
